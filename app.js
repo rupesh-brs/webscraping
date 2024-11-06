e6a2ae150d20b63531aae537320019f1098be54e
rupesh-brs <alajangirupesh@gmail.com>
2024-11-06 12:46:36 +0530

    change at port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 const path = require("path");
 const app = express();
-const port = 8000;
+const port = 8080;
 
 app.set("view engine", "ejs"); 
 app.set("views", path.join(__dirname, "views")); 
